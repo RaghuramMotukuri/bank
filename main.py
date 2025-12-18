f3cb7a0660482965a4e4c8a58dd01a1a60649285
RaghuramMotukuri <raghurammotukuri@gmail.com>
2025-12-18 21:20:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,38 @@ import streamlit as st
 import pandas as pd
 from supabase import create_client
 
-#supabase configuration
-supabase_url="https://hzfsuubrxnammebwaotm.supabase.co"
-supabase_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6ZnN1dWJyeG5hbW1lYndhb3RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDMzODEsImV4cCI6MjA4MTYxOTM4MX0.BGVb89liDXpQ_ZC-DWHrA4SPyQ3V4YZwZWRgP2huclk"
+#//supabase configuration
+
+supabase_url="https://supabase.co/"
+supabase_key="sb_publishable_m9Tal4APplFeDu2kh5gQ5A_jtz9vKJI "
+
 supabase=create_client(supabase_url,supabase_key)
-#streamlit UI
-st.title("HDFC(Supabase)")
-####################################
+
+#//streamlit UI
+
+st.title("HDFC BANK(Supabase)")
+
 menu=("REGISTER","VIEW")
 choice=st.sidebar.selectbox("Menu",menu)
-#Register
-if choice=="Register":
+
+
+#//REGISTER
+
+if choice=="REGISTER":
     name=st.text_input("enter name")
-    age=st.number_input("Age",min_Value=18)
-    account=int(st.number_input("Account number"))
+    age=st.number_input("AGE",min_value=18)
+    account=int(st.number_input("ACCOUNT NUMBER"))
     bal=st.number_input("BALANCE",min_value=500)
-    if st.button("save"):
+    if st.button("SAVE"):
         supabase.table("users").insert({
             "name":name,
             "age":age,
             "account":account,
             "balance":bal}).execute()
         st.success("user added successfully")
-#view students
-#--------------------------------------------
-if choice=="view":
-    st.subheader("view header")
-    data=supabase.table("users").select("*").execute()
+
+#// view student        
+if choice == "view":
+    st.subheader(" view user")
+    data= supabase.table("users").select("*").execute()
     df=pd.DataFrame(data.data)
